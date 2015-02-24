315ec26968185a833036ec493b495c1e977646cf
JRH2794 <jrh2794@email.vccs.edu>
2015-02-23 23:22:25 -0500

    First/Second

--- src/Assignments/Book/Book.java
+++ src/Assignments/Book/Book.java
@@ -3,17 +3,15 @@ public class Book {
 
     String Title, Author; int NoP;
 
-// Constructor and toString() Methods:
-
     public Book () {
         Title = "None";
         Author = "None";
         NoP = 0; }
 
-    public Book (String newAuthor, String newTitle, int newNoP) {
-        Author = newAuthor;
-        Title = newTitle;
-        NoP = newNoP; }
+    public Book (String newA, String newT, int newN) {
+        Author = newA;
+        Title = newT;
+        NoP = newN; }
 
     public String toString() {
        return "Title: " + Title + "\nAuthor: " + Author + "\nNumber of Pages: " + NoP; } }
