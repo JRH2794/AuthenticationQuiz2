a080be0a45547b78625d9e7bac79ac0e15501869
JRH2794 <jrh2794@email.vccs.edu>
2015-03-05 15:03:48 -0500

    first commit

---
+++ src/Assignments/Book/Cycle.java
@@ -0,0 +1,15 @@
+package Assignments.Book;
+
+public class Cycle {
+    private int numberOfWheels, weight;
+
+public Cycle() { // Default using 'this'.
+    this(100, 1000); }
+
+public Cycle(int nNumberOW, int nWeight) { // Contructor with arguments.
+    numberOfWheels = nNumberOW;
+    weight = nWeight; }
+
+public String toString() {
+    return "Weight: " + weight + "\n" +
+            "Number of Wheels: " + numberOfWheels; } }
